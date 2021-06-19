6db99678c75dd34f8dc3fe7d84b10e99495b5207
Evan McCullough <evanmccullough@evans-air.lan>
2021-06-19 00:20:51 -0400

    update user input to allow working with multiple files in single program run

--- edr_test/file_operations.rb
+++ edr_test/file_operations.rb
@@ -10,7 +10,7 @@ module EDRTest
       @logger = EDRTest::Logger::FileOperationsDecorator.new(EDRTest::Logger::ProcessLogger.new)
     end
 
-    attr_accessor :logger, :logfile
+    attr_accessor :logger, :logfile, :full_filename
 
     def create_file
       new_file = File.open(@full_filename, 'w')
@@ -35,29 +35,14 @@ module EDRTest
     end
 
     def self.prompt_user
-      puts <<-MSG
-        Welcome to the file operations simulator! Using this utility, you can input a file type
-        and a file name or path (including filename) and perform the following operations with
-        that file:
+      welcome_user
 
-        1) Create the file (if it doesn't already exist)
-        2) Provide contents to write to the file (does nothing if the file does not exist)
-        3) Delete the file (does nothing if the file does not exist)\n
-      MSG
-
-      puts <<-TYPE
-        What type of file would you like to create? (Enter the file extension without the '.'- e.g. 
-        `rb`, instead of `.rb`):
-      TYPE
-      file_type = gets.chomp
-
-      puts 'What is the filename or path + filename you wish to create?'
-      file_path = gets.chomp
-
+      file_path, file_type = user_file_details
       my_file = new(path: file_path, filetype: file_type)
       my_file.logfile.start_log
 
-      puts 'Thank you! Which operation do you wish to perform? 1) Create 2) Modify 3) Delete X) Exit'
+      puts 'Thank you! Which operation do you wish to perform?'
+      puts '1) Create 2) Modify 3) Delete X) Exit'
       next_operation = gets.chomp
 
       while next_operation.upcase != 'X'
@@ -70,15 +55,48 @@ module EDRTest
           my_file.modify_file(contents: contents)
         when '3'
           my_file.delete_file
+        when '4'
+          path, filetype = user_file_details
+          my_file.full_filename = path[-1] == '/' ? "#{path}test.#{file_type}" : "#{path}.#{filetype}"
         else
           puts 'Sorry, that seems to be an invalid input!'
         end
 
-        puts 'Thank you! Which operation do you wish to perform? 1) Create 2) Modify 3) Delete X) Exit'
+        puts 'Thank you! Which operation do you wish to perform?'
+        puts '1) Create 2) Modify 3) Delete 4) Set a new file X) Exit'
         next_operation = gets.chomp
       end
 
       my_file.logfile.stop_log
+    end
+
+    private
+
+    class << self
+      def welcome_user
+        puts <<-MSG
+          Welcome to the file operations simulator! Using this utility, you can input a file type
+          and a file name or path (including filename) and perform the following operations with
+          that file:
+
+          1) Create the file (if it doesn't already exist)
+          2) Provide contents to write to the file (does nothing if the file does not exist)
+          3) Delete the file (does nothing if the file does not exist)\n
+        MSG
+      end
+
+      def user_file_details
+        puts <<-TYPE
+          What type of file would you like to create? (Enter the file extension without the '.'- e.g. 
+          `rb`, instead of `.rb`):
+        TYPE
+        file_type = gets.chomp
+
+        puts 'What is the filename or path + filename you wish to create?'
+        file_path = gets.chomp
+
+        [file_path, file_type]
+      end
     end
   end
 end
