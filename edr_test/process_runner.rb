72e5d5bb7e0ffde9aa403ca196454a506d670bfe
Evan McCullough <evanmccullough@evans-air.lan>
2021-06-19 00:58:02 -0400

    implement user prompts to run processes

--- edr_test/process_runner.rb
+++ edr_test/process_runner.rb
@@ -17,12 +17,33 @@ module EDRTest
       @logfile.write(@logger.build_log(pid))
       Process.detach(pid)
     end
+
+    def self.prompt_user
+      puts 'Welcome! This utility exists to test running various processes, such as running a'\
+        " Ruby program, executing a git command, or checking out a man page!\n\n"\
+        'Please enter the command you wish to run, along with any relevant arguments:'
+
+      next_command = gets.chomp
+
+      pr = EDRTest::ProcessRunner.new
+      pr.logfile.start_log
+
+      while next_command.downcase != 'exit'
+        pr.run_process(next_command)
+
+        puts "Your command has been run and logged! If you would like to run another, you may\n"\
+          ' enter the command, along with any relevant arguments, or type "exit" to quit: '
+        next_command = gets.chomp
+
+        puts next_command
+      end
+
+      pr.logfile.stop_log
+
+    rescue StandardError
+      pr.logfile.stop_log
+    end
   end
 end
 
-pr = EDRTest::ProcessRunner.new
-pr.logfile.start_log
-
-pr.run_process
-
-pr.logfile.stop_log
+EDRTest::ProcessRunner.prompt_user
