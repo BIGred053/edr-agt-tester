6f8e577a18988f7f5d16178058bde108dff6fdfa
Evan McCullough <evan.m.mccullough@gmail.com>
2021-06-22 14:11:54 -0400

    address typo in base logger class

--- edr_test/logger/base_logger.rb
+++ edr_test/logger/base_logger.rb
@@ -2,7 +2,7 @@ module EDRTest
   module Logger
     class BaseLogger
       def build_log(pid)
-        rails NotImplementedError, "#{self.class} has not implemented the #{__method__} method"
+        raise NotImplementedError, "#{self.class} has not implemented the #{__method__} method"
       end
     end
   end
